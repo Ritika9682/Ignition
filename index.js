93b217d76b47be8ea71e025b51e0557412130e8e
Ritika <ritikatharmatt@gmail.com>
2023-01-03 21:27:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', () => console.log("Error in Connecting to Database"));
+db.once('open', () => console.log("Connected to Database"))
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body
+    User.findOne({ email: email }, (err, user) => {
+        if (user) {
+            if (password === user.password) {
+                // res.send({message: "Login Successfull", user: user})
+                res.redirect('signup_success.html')
+                // return res.redirect('signup_success.html')
+            } else {
+                res.send({ message: "Password didn't match" })
+            }
+        } else {
+            res.send({ message: "User not registered" })
+        }
+    })
+})
+
+
+
+app.post("/sign_up", (req, res) => {
+    const { name, email, password } = req.body
+    User.findOne({ email: email }, (err, user) => {
+        if (user) {
+            res.send({ message: "User already registerd" })
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            user.save(err => {
+                if (err) {
+                    res.send(err)
+                } else {
+                    return res.redirect('signup_success.html')
+                    // res.send( { message: "Successfully Registered, Please login now." })
+                }
+            })
+        }
+    })
+
+})
+
+
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+console.log("Listening on PORT 3000");
+
